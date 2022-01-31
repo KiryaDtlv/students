ceee65bb353d45ac1222705fe91335e5dcc31f85
KiryaDtlv <dyatlov.kiryuha@gmail.com>
2022-01-31 13:00:11 +0700

    mamba new project

---
+++ run.py
@@ -0,0 +1,4 @@
+from stundents import start
+
+if __name__ == "__main__":
+    start()
---
+++ stundents/__init__.py
@@ -0,0 +1,16 @@
+# -*- encoding=utf-8 -*-
+# This file was generated automatically
+
+__version__ = (1,0,0,0)
+
+def start():
+    try:
+        print("stundents v"+".".join(str(x) for x in __version__))
+        # YOUR CODE HERE
+        pass
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    start()
+                
